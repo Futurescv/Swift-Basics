e9cdeedcfd9f59a666c2285236dd592b22894ba5
123 <a123@123demacbook.local>
2018-08-29 09:12:17 -0500

    Initial Commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by 123 on 2018/8/29.
+//  Copyright © 2018年 Jin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
